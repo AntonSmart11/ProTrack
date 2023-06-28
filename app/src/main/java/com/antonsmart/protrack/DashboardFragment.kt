dddb14b2b43949affcacfdb52a6b8578d53c2394
AntonSmart <antoniooc11@outlook.com>
2023-06-28 10:18:05 -0600

    Se compuso el NavigationDrawer

--- app/src/main/java/com/antonsmart/protrack/DashboardFragment.kt
+++ app/src/main/java/com/antonsmart/protrack/DashboardFragment.kt
@@ -8,27 +8,25 @@ import android.view.ViewGroup
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import com.google.android.material.navigation.NavigationView
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
-    private lateinit var toolbar: Toolbar
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Find the toolbar
-        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar = view.findViewById(R.id.toolbar)
 
-        // Set the toolbar as the action bar
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
 
-        // Enable the navigation icon
-        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // Set up the navigation drawer
         val drawerLayout = view.findViewById<DrawerLayout>(R.id.drawerLayout)
         drawerToggle = ActionBarDrawerToggle(
             requireActivity(),
@@ -37,14 +35,35 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close
         )
+        drawerToggle.setHomeAsUpIndicator(R.drawable.menu)
+        drawerToggle.drawerArrowDrawable.color = ContextCompat.getColor(requireContext(), R.color.white)
         drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
 
-        // Set up the navigation view
         val navigationView = view.findViewById<NavigationView>(R.id.navigationView)
-        navigationView.setNavigationItemSelectedListener(this)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle navigation item clicks here
+            when (menuItem.itemId) {
+                R.id.project -> {
+                    Navigation.findNavController(requireView())
+                        .navigate(R.id.action_dashboardFragment_to_loginFragment)
+                }
+                R.id.notes -> {
+                    Navigation.findNavController(requireActivity(), R.id.loginFragment)
+                        .navigate(R.id.loginFragment)
+                }
+                R.id.roles -> {
+                    Navigation.findNavController(requireActivity(), R.id.loginFragment)
+                        .navigate(R.id.loginFragment)
+                }
+                R.id.remembers -> {
+                    Navigation.findNavController(requireActivity(), R.id.loginFragment)
+                        .navigate(R.id.loginFragment)
+                }
+            }
+            true
+        }
 
-        return view
     }
 
 }
