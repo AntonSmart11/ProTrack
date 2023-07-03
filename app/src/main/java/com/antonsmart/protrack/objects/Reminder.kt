5a9b25edf4c7ea956ce2f4450fd04a39be61bcbc
Miguel <miguelgerardosalvadorperez@gmail.com>
2023-07-03 10:41:12 -0600

    Archivos faltantes del commit anterior (objects,xml,adaptadores y demas)

---
+++ app/src/main/java/com/antonsmart/protrack/objects/Reminder.kt
@@ -0,0 +1,5 @@
+package com.antonsmart.protrack.objects
+
+import java.util.Date
+
+data class Reminder(var title:String,val text:String,val date: Date)
---
+++ app/src/main/java/com/antonsmart/protrack/objects/Role.kt
@@ -0,0 +1,3 @@
+package com.antonsmart.protrack.objects
+
+class Role(var title: String)
