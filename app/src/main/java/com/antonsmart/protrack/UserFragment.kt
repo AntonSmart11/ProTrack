d62fb107b7c8eadcb2ac9822851521f97b8aef19
AntonSmart <antoniooc11@outlook.com>
2023-07-16 11:26:08 -0600

    Se agregó la modificación de los datos de la base de datos a la página del usuario

--- app/src/main/java/com/antonsmart/protrack/UserFragment.kt
+++ app/src/main/java/com/antonsmart/protrack/UserFragment.kt
@@ -8,6 +8,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.antonsmart.protrack.database.SQLiteHelper
 import com.antonsmart.protrack.databinding.FragmentUserBinding
+import com.antonsmart.protrack.global.Global
+import com.antonsmart.protrack.objects.User
 
 class UserFragment : Fragment(R.layout.fragment_user) {
 
@@ -18,20 +20,30 @@ class UserFragment : Fragment(R.layout.fragment_user) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = DataBindingUtil.bind(view)!!
-
-
         sqliteHelper = SQLiteHelper(requireContext())
+
+        //Global variable
+        val idUser = Global.idUser
+
+        //Get user
+        val user = user(idUser)
+
+        //Content
+        binding.userName.setText(user.name)
+        binding.userLast.setText(user.last)
+        binding.userUsername.setText(user.username)
+        binding.userPassword.setText(user.password)
 
         binding.arrowBack.setOnClickListener {
             requireActivity().onBackPressed()
         }
     }
 
-    private fun user(id: Int) {
+    private fun user(id: Int): User {
         val userList = sqliteHelper.GetAllUsers()
         val user = userList.find { it.id == id }
 
-
+        return user!!
     }
 
 }
